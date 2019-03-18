28b3b40c96111aa47f3958dc5df949a0a8fa0f59
pruthvibhaskar <pruthvi.bhaskar@gmail.com>
2019-03-18 01:06:46 -0700

    Add files via upload

--- ProgrammingAssignment2.R
+++ ProgrammingAssignment2.R
@@ -1,34 +1,39 @@
-#Matrix to catch Inverse
+#Matrix to catch the Inverse
 makeCacheMatrix <- function(x = matrix())
 {
-  mat_inv <- NULL
+  inv <- NULL
   set <- function(y)
   {
+    
     x <<- y
-    mat_inv <<- NULL
+    inv <<- NULL
+    
   }
   get <- function() x
   setInverse <- function(inverse)
   {
-    mat_inv <<- inverse
+    
+    inv <<- inverse
+    
   }
   getInverse <- function() inv
-  list(set = set, get = get,
+  list(set = set, get = get, 
        setInverse = setInverse, getInverse = getInverse)
 }
 
-
-#Matrix to compute Inverse
+#Matrix to compute the Inverse
 cacheSolve <- function(x,...)
 {
-  mat_inv <- x$getInverse()
+  inv <- x$getInverse()
   if(!is.null(inv))
   {
+    
     message("getting catched data")
     return(inv)
+    
   }
   mat <- x$get()
-  mat_inv <- solve(mat,...)
-  x$setInverse(mat_inv)
-  mat_inv
+  inv <- solve(mat,...)
+  x$setInverse(inv)
+  inv
 }
